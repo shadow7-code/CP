bade712c4292459914ddb7c541a452fdd052951e
shadow7-code <pranavjain72007@gmail.com>
2026-01-09 21:22:35 +0530

    1901A

---
+++ A_Line_Trip.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int max(int a,int b){ return a>b?a:b; }
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        long long n, x;
+        cin >> n >> x;
+        vector<long long> arr;
+        arr.push_back(0);
+        for(int i = 0; i < n; i++){
+            long long d;
+            cin >> d;
+            arr.push_back(d);
+        }
+        arr.push_back(x);
+        
+        n=arr.size();
+        long long res = 0;
+        for(int i = 1; i < n; i++){
+            long long gap = arr[i] - arr[i - 1];
+            if (i == n-1)    res = max(res, 2 * gap);
+            else             res = max(res, gap);
+        }
+        cout << res << "\n";
+    }
+    return 0;
+}
