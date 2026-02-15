55d3106239720d89275eea16dc2555677ff5a356
shadow7-code <pranavjain72007@gmail.com>
2026-02-15 13:54:32 +0530

    1537B

---
+++ B_Bad_Boy.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, m, i, j;
+		cin >> n >> m >> i >> j;
+		cout << 1 << " " << 1 << " " << n << " " << m << "\n";
+    }
+    return 0;
+}
