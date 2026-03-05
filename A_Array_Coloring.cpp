978079a6a482e66d01627f7c104a01a243c6602f
shadow7-code <pranavjain72007@gmail.com>
2026-03-05 20:08:02 +0530

    2191A

--- A_Array_Coloring.cpp
+++ A_Array_Coloring.cpp
@@ -2,22 +2,21 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
 
-        int odd = 0;
-        for (int i = 0; i < n; i++) {
-            long long x;
-            cin >> x;
-            odd += (x & 1);
+        vector<int> arr(n);
+        for (int &x : arr)   cin >> x;
+        bool flag = true;
+        for (int i = 1; i < n; i++) {
+            if (!((arr[i] ^ arr[i-1]) & 1)) {
+                flag = false;
+                break;
+            }
         }
-        cout << (odd % 2 ? "NO\n" : "YES\n");
+        cout << (flag ? "YES\n" : "NO\n");
     }
-    return 0;
 }
