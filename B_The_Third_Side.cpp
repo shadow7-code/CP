f162931c4616e70d56ee28d99f339199fd260a5f
shadow7-code <pranavjain72007@gmail.com>
2026-04-03 20:11:35 +0530

    2074B

---
+++ B_The_Third_Side.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        long long sum=0,x;
+        for(int i=0;i<n;i++){
+            cin>>x;
+            sum+=x;
+        }
+        cout<<sum-(n-1)<<"\n";
+    }
+}
