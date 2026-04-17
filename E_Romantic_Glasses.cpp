ee415bbdaae6eeaa6a0800784c65e099f7f93271
shadow7-code <pranavjain72007@gmail.com>
2026-04-17 21:45:56 +0530

    1915E

---
+++ E_Romantic_Glasses.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+
+        vector<long long> a(n);
+        for(int i = 0; i < n; i++)  cin >> a[i];
+        unordered_set<long long> st;
+        long long even = 0, odd = 0;
+        bool flag = false;
+
+        st.insert(0);
+        for(int i = 0; i < n; i++) {
+            if(i % 2 == 0)  even += a[i];
+            else            odd += a[i];
+
+            long long diff = even - odd;
+            if(st.count(diff)) {
+                flag = true;
+                break;
+            }
+            st.insert(diff);
+        }
+        cout << (flag ? "YES\n" : "NO\n");
+    }
+}
