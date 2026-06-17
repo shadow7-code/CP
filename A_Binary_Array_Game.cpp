0d11fab892b76a01412266859b1bd397a8fe2225
shadow7-code <pranavjain72007@gmail.com>
2026-06-17 19:45:37 +0530

    2183A

---
+++ A_Binary_Array_Game.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++)   cin>>arr[i];
+        if(arr[0]==0 && arr[n-1]==0)    cout<<"Bob\n";
+        else                            cout<<"Alice\n";
+    }
+    return 0;
+}
